7c10cbafd1acc4e4aa0e32f7a916d2183ddf61b2
sky-adams <skyadams@sbunified.org>
2017-10-08 18:05:57 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,8 +6,17 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-def render_result():
-    return render_template('result.html')
+@app.route("/response")
+def render_response():
+    color = request.args['color'] 
+    #The request object stores information about the request sent to the server.
+    #args is a MultiDict (like a dictionary but can have mutliple values for the same key)
+    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
+    if color == 'pink':
+        reply = "That's my favorite color, too!"
+    else:
+        reply = "My favorite color is pink."
+    return render_template('response.html', response = reply)
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
