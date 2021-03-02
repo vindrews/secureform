d7c8cefd0ccb9fb957c714db505e336603bbbe94
sky-adams <skyadams@sbunified.org>
2021-03-02 06:05:14 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,7 +10,7 @@ def render_main():
 def render_response():
     color = request.args['color'] 
     #The request object stores information about the request sent to the server.
-    #args is a MultiDict (like a dictionary but can have mutliple values for the same key)
+    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if color == 'pink':
         reply = "That's my favorite color, too!"
