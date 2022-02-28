757f17e47b26bd5e0e6286a9bed30130be7800f1
P1 <you@example.com>
2022-02-28 09:53:22 -0800

    started making it

--- webapp.py
+++ webapp.py
@@ -8,17 +8,14 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    color = request.args['color'] 
+    name = request.args['name'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply1 = "That's my favorite color, too!"
+    if name == 'talkbot':
+        reply1 = "That's my name, too! What a nice coincidence :] How are you today, Talkbot?"
     else:
-        reply1 = "My favorite color is pink."
-    n = int(request.args['multNum']) #values in request.args are strings by default
-    reply2 = "2 x " + str(n) + " = " + str((2*n))
-    return render_template('response.html', response1 = reply1, response2 = reply2)
+        reply1 = "What a lovely name! How are you doing today, name?"
     
 if __name__=="__main__":
     app.run(debug=False)
