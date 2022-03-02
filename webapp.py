6b634941c1c0a949679b9b5280f804a2ccefd00b
P1 <you@example.com>
2022-03-02 09:56:12 -0800

    yeah. added some new generated password templates

--- webapp.py
+++ webapp.py
@@ -8,12 +8,22 @@ def render_main():
 
 @app.route("/response")
 def render_response():
+import random
+    a = random.randrange(1, 100)
     word = input('word') 
     number = input('number')
+    
+if a >= 1 and a < 10:
     return("*()*"+word[0:3]+number+word[3:5]+"!@!")
+elif a >=10 and a < 20:
+    return("+{}+"+word[0:3]+number+word[3:5]+"*$*")
+elif a >= 20 and a < 30:
+    return("~()~"+word[3:5]+number+word[0:3]+"=!=")
+elif a >= 30 and a < 40:
+    return("^&^"+word[3:5]+number+wprd[0:3]+">.<!")
+elif a >= 40 and a <50
  
         
 render_response()
 render_main()
-        
         
