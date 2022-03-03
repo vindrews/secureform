04bd22408cc94afa8757c5024ae49c73d67fac64
P1 <you@example.com>
2022-03-03 10:19:48 -0800

    fixed the stuff i needed to!

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, render_template, request
+import random
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -8,22 +9,31 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-import random
     a = random.randrange(1, 100)
-    word = input('word') 
-    number = input('number')
+    word = request.args['word']
+    number = request.args['number']
     
-if a >= 1 and a < 10:
-    return("*()*"+word[0:3]+number+word[3:5]+"!@!")
-elif a >=10 and a < 20:
-    return("+{}+"+word[0:3]+number+word[3:5]+"*$*")
-elif a >= 20 and a < 30:
-    return("~()~"+word[3:5]+number+word[0:3]+"=!=")
-elif a >= 30 and a < 40:
-    return("^&^"+word[3:5]+number+wprd[0:3]+">.<!")
-elif a >= 40 and a <50
- 
-        
-render_response()
-render_main()
-        
+    if a >= 1 and a < 10:
+        response = "*()*"+word[0:3]+number+word[3:10]+"!@!"
+    elif a >=10 and a < 20:
+        response = "+{}+"+word[0:3]+number+word[3:10]+"*$*"
+    elif a >= 20 and a < 30:
+        response = "~()~"+word[3:10]+number+word[0:3]+"=!="
+    elif a >= 30 and a < 40:
+        response = "^&^"+word[3:10]+number+word[0:3]+">.<!"
+    elif a >= 40 and a < 50:
+        response = "=+=+"+word[3:5]+number+word[0:10]+"!.!=]"
+    elif a >= 50 and a < 60:
+        response = word[3:10]+"!_!"+number+"!@#@!"+word[0:3]
+    elif a >= 60 and a < 70:
+        response = word[0:3]+"-_-"+number+"{}_{}"+word[3:10]
+    elif a >= 70 and a < 80:
+        response = word[3:10]+"{}-{}"+number+"!%%!"+word[0:3]
+    elif a >= 80 and a < 90:
+        response = word[3:10]+"^%%^"+number+"+:##:+"+word[0:3]
+    elif a >-90 and word <= 100:
+        reponse = word[0:3]+"("+number+")"+"| |"+word[3:10]
+    return render_template('response.html', response = response)
+
+if __name__=="__main__":
+    app.run(debug=False)
