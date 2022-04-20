7f274b068a581e38a0731503cd514634d5f1f4f2
P1 <you@example.com>
2022-04-20 09:51:42 -0700

    argh. i still need to get the response working and add css

--- webapp.py
+++ webapp.py
@@ -9,6 +9,7 @@ app.secret_key=os.environ["SECRET_KEY"];
 
 @app.route("/")
 def render_main():
+
     return render_template('home.html')
 
 @app.route("/page1", methods=['POST', 'GET'])
@@ -28,41 +29,24 @@ def render_page3():
     
 @app.route("/page4", methods=['POST', 'GET'])
 def render_page4():
-session["playername"]=request.form['playername']
+    session["playername"]=request.form['playername']
     return render_template('page4.html')
 
 @app.route("/response", methods=['POST', 'GET'])
 def render_response():
-    a = random.randrange(1, 100)
-    word = request.form['word']
-    number = request.form['number']
-    response = ""
+    session["countto"]=request.form['countto']
+    game = session['1stgame']
+    players = session['players']
+    engine = session['gameengine']
+    name = session['playername']
+    countto = session['countto']
 
-    if request.method == 'POST':
-        if a >= 1 and a < 10:
-            response = "*()*"+word[0:3]+number+word[3:10]+"!@!"
-        elif a >=10 and a < 20:
-            response = "+{}+"+word[0:3]+number+word[3:10]+"*$*"
-        elif a >= 20 and a < 30:
-            response = "~()~"+word[3:10]+number+word[0:3]+"=!="
-        elif a >= 30 and a < 40:
-            response = "^&^"+word[3:10]+number+word[0:3]+">.<!"
-        elif a >= 40 and a < 50:
-            response = "=+=+"+word[3:5]+number+word[0:10]+"!.!=]"
-        elif a >= 50 and a < 60:
-            response = word[3:10]+"!_!"+number+"!@#@!"+word[0:3]
-        elif a >= 60 and a < 70:
-            response = word[0:3]+"-_-"+number+"{}_{}"+word[3:10]
-        elif a >= 70 and a < 80:
-            response = word[3:10]+"{}-{}"+number+"!%%!"+word[0:3]
-        elif a >= 80 and a < 90:
-            response = word[3:10]+"^%%^"+number+"+:##:+"+word[0:3]
-        elif a >= 90 and word <= 100:
-            reponse = word[0:3]+"("+number+")"+"| |"+word[3:10]
-        return render_template('response.html', response = response)
+    
+    if game == "halflife1" and players == "9" and engine == "source" and name == "chell" and countto == "3":
+        reponse = "Great job! You know your Valve games. You got 5/5 correct!"
     else:
-        response = "Uh oh! Something went wrong, try again."
-        return render_template('response.html', response = response)
+        response = "Aw, you got some wrong! Try again?" 
+    return render_template('response.html', response = response)
 
 if __name__=="__main__":
     app.run(debug=False)
