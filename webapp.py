464632aba295936e3ec42b1f60e18c5efd33b949
P1 <you@example.com>
2022-04-21 10:20:40 -0700

    woo!!

--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
+from markupsafe import Markup, escape
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -9,7 +10,6 @@ app.secret_key=os.environ["SECRET_KEY"];
 
 @app.route("/")
 def render_main():
-
     return render_template('home.html')
 
 @app.route("/page1", methods=['POST', 'GET'])
@@ -35,18 +35,18 @@ def render_page4():
 @app.route("/response", methods=['POST', 'GET'])
 def render_response():
     session["countto"]=request.form['countto']
-    game = session['1stgame']
-    players = session['players']
-    engine = session['gameengine']
-    name = session['playername']
-    countto = session['countto']
+    response = ""
 
-    
-    if game == "halflife1" and players == "9" and engine == "source" and name == "chell" and countto == "3":
-        reponse = "Great job! You know your Valve games. You got 5/5 correct!"
-    else:
-        response = "Aw, you got some wrong! Try again?" 
+    if session['1stgame'] == "Half Life 1" and session['players'] == "9" and session['gameengine'] == "Source" and session['playername'] == "Chell" and session['countto'] == "3":
+        response = response + "Great job! You know your Valve games. You got 5/5 correct!"
+    elif session['1stgame'] != "Half Life 1" or session['players'] != "9" or session['gameengine'] != "Source" or session['playername'] != "Chell" or session['countto'] != "3": 
+        response = response + Markup("You answered some questions wrong. You said that Valve's first game was " + session['1stgame'] + ", that there were "+ session['players'] + " playable characters in Team Fortress Two, that Valve's game engine was " + session['gameengine'] + ", that the player's name in Portal is " + session['playername'] + ", and that Valve can't count to " + session['countto'] + ". One or more of these was wrong. Would you like to try again?") 
     return render_template('response.html', response = response)
 
+@app.route('/startOver')
+def startOver():
+    session.clear() 
+    return redirect(url_for('render_main')) 
+    
 if __name__=="__main__":
     app.run(debug=False)
